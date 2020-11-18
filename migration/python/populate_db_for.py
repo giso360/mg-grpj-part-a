6994d6b205e8b6b5e7c55ddb17a5f52b76b77be3
Maria <marousopoulou@gmail.com>
2020-11-18 23:18:28 +0200

    Populate DB with local_infile

--- migration/python/populate_db_for.py
+++
@@ -1,2 +0,0 @@
-import pymysql
-import pandas as pd
--- migration/python/populate_db_infile.py
+++ migration/python/populate_db_infile.py
@@ -1,60 +1,52 @@
 import pymysql
 
-show_variables = """SHOW VARIABLES LIKE '%LOCAL%'"""
-set_global = """SET GLOBAL local_infile=1"""
 show_wornings = """SHOW warnings"""
     
 
 # Open db connection
 db = pymysql.connect(host = "localhost",
                      user = "root",
-                     passwd = "root")
+                     passwd = "root",
+                     local_infile = True)
 cursor = db.cursor()
 
 cursor.execute("use books")
 
 
 # Populate books_table
-populate_books = """LOAD DATA LOW_PRIORITY LOCAL INFILE 'C:\\Users\\marou\\OneDrive\\Υπολογιστής\\MS in Data Science\\Fall 2020\\Introduction to Big Data\\Team Project\\Git\\Part A\\mg-grpj-part-a\\migration\\books_to_db.csv'
-                    INTO TABLE `books`.`books_table`
-                    CHARACTER SET latin1
-                    FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r\n'
-                    IGNORE 1 LINES (`ISBN`, `Book-Title`, `Book-Author`, `year`, `Publisher`)"""
-cursor.execute(show_variables)
-cursor.execute(set_global)
-cursor.execute(show_variables)
-cursor.execute(populate_books)
-db.commit()
-cursor.execute(show_wornings)
+# populate_books = """LOAD DATA LOW_PRIORITY INFILE 'books_to_db.csv'
+#                     INTO TABLE books.books_table
+#                     CHARACTER SET latin1
+#                     FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n'
+#                     IGNORE 1 LINES (`ISBN`, `Book-Title`, `Book-Author`, `year`, `Publisher`)"""
+
+# cursor.execute(populate_books)
+# db.commit()
+# cursor.execute(show_wornings)
 
 
 # Populate users
-populate_users = """LOAD DATA LOW_PRIORITY LOCAL INFILE 'C:\\Users\\marou\\OneDrive\\Υπολογιστής\\MS in Data Science\\Fall 2020\\Introduction to Big Data\\Team Project\\Git\\Part A\\mg-grpj-part-a\\migration\\users_to_db.csv'
-                    INTO TABLE `books`.`users`
-                    CHARACTER SET latin1
-                    FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r\n'
-                    IGNORE 1 LINES (`userid`, `location`, `age`)"""
-cursor.execute(show_variables)
-cursor.execute(set_global)
-cursor.execute(show_variables)
-cursor.execute(populate_users)
-db.commit()
-cursor.execute(show_wornings)
+# populate_users = """LOAD DATA LOW_PRIORITY INFILE 'users_to_db.csv'
+#                     INTO TABLE books.users
+#                     CHARACTER SET latin1
+#                     FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n'
+#                     IGNORE 1 LINES (`userid`, `location`, `age`)"""
+                    
+# cursor.execute(populate_users)
+# db.commit()
+# cursor.execute(show_wornings)
 
 
 # Populate ratings_table
-populate_ratings = """LOAD DATA LOW_PRIORITY LOCAL INFILE 'C:\\Users\\marou\\OneDrive\\Υπολογιστής\\MS in Data Science\\Fall 2020\\Introduction to Big Data\\Team Project\\Git\\Part A\\mg-grpj-part-a\\migration\\ratings_to_db.csv'
-                      INTO TABLE `books`.`ratings_table`
-                      CHARACTER SET latin1
-                      FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r\n'
-                     IGNORE 1 LINES (`User-ID`, `ISBN`, `Book-Rating`)"""
+# populate_ratings = """LOAD DATA LOW_PRIORITY INFILE 'ratings_to_db.csv'
+#                       INTO TABLE books.ratings_table
+#                       CHARACTER SET latin1
+#                       FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n'
+#                       IGNORE 1 LINES (`User-ID`, `ISBN`, `Book-Rating`)"""
                      
-cursor.execute(show_variables)
-cursor.execute(set_global)
-cursor.execute(show_variables)
-cursor.execute(populate_ratings)
-db.commit()
-cursor.execute(show_wornings)
+# cursor.execute(populate_ratings)
+# db.commit()
+# cursor.execute(show_wornings)
 
 
 # Close db connection
