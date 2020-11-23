256c5953598253039497627d022c1e4cb4e40936
Maria <marousopoulou@gmail.com>
2020-11-23 21:17:09 +0200

    Copy files to database path

--- migration/python/populate_db_infile.py
+++ migration/python/populate_db_infile.py
@@ -1,7 +1,19 @@
 import pymysql
+import shutil
 
-show_wornings = """SHOW warnings"""
-    
+
+# File's original paths
+books_original_path = """../books_to_db.csv"""
+users_original_path = """../users_to_db.csv"""
+ratings_original_path = """../ratings_to_db.csv"""
+
+
+# MySQL queries
+show_warnings = """SHOW warnings"""
+get_path = """SELECT @@datadir"""
+wait_timeout = """SET innodb_lock_wait_timeout = 120"""
+use_db = """use books"""
+
 
 # Open db connection
 db = pymysql.connect(host = "localhost",
@@ -9,10 +21,24 @@ db = pymysql.connect(host = "localhost",
                      passwd = "root",
                      local_infile = True)
 cursor = db.cursor()
-cursor.execute("SET innodb_lock_wait_timeout = 120")
-cursor.execute("use books")
 
 
+# Settings
+cursor.execute(wait_timeout)
+
+
+# Get path
+cursor.execute(get_path)
+# destination_path = [item[0] for item in cursor.fetchall()]
+destination_path = """C:/ProgramData/MySQL/MySQL Server 8.0/Data/books"""
+
+copy_books = shutil.copy(books_original_path, destination_path)
+copy_users = shutil.copy(users_original_path, destination_path)
+copy_ratings = shutil.copy(ratings_original_path, destination_path)
+
+
+# Use database
+cursor.execute(use_db)
 
 # Populate books_table
 populate_books = """LOAD DATA LOW_PRIORITY INFILE 'books_to_db.csv'
@@ -23,7 +49,7 @@ populate_books = """LOAD DATA LOW_PRIORITY INFILE 'books_to_db.csv'
 
 cursor.execute(populate_books)
 db.commit()
-cursor.execute(show_wornings)
+cursor.execute(show_warnings)
 
 
 # Populate users
@@ -35,7 +61,7 @@ populate_users = """LOAD DATA LOW_PRIORITY INFILE 'users_to_db.csv'
                     
 cursor.execute(populate_users)
 db.commit()
-cursor.execute(show_wornings)
+cursor.execute(show_warnings)
 
 
 # Populate ratings_table
@@ -47,7 +73,7 @@ populate_ratings = """LOAD DATA LOW_PRIORITY INFILE 'ratings_to_db.csv'
                      
 cursor.execute(populate_ratings)
 db.commit()
-cursor.execute(show_wornings)
+cursor.execute(show_warnings)
 
 # Close db connection
 db.close()
